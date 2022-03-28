ba87edd523ec00fbbfe3174d444e0bd083c1ff0d
AndreaGir <girolamiandrea2000@gmail.com>
2022-03-28 15:48:10 +0200

    comm

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php 
+
+    $paragrafo = 'Lorem ipsum, cacca sit amet consectetur adipisicing cacca.
+     Omnis eius, illo dolorum tempore cacca tenetur quaerat laudantium provident cacca veniam. cacca.';
+
+    $lunghezzaParagrafo = strlen( $paragrafo );
+
+    $paragrafoCensurato = str_replace( 'cacca', '***', $paragrafo , $numeroCensure);
+
+    $numeroCensure = 0;
+
+    $pHost = $_GET['pHost']
+
+?>
+
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall’utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +26,23 @@
     <title>Document</title>
 </head>
 <body>
+
+    <p>
+        <?php echo $paragrafo ?>
+    </p>
+    <p>
+        Lunghezza paragrafo: <?php echo $lunghezzaParagrafo ?> caratteri
+    </p>
+    <p>
+        <?php echo $paragrafoCensurato ?>
+    </p>
+    <p>
+        Numero censure: <?php echo $numeroCensure ?> parole
+    </p>
+    <p>
+    <?php echo $pHost ?>
+    </p>
+    
     
 </body>
 </html>
